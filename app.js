2ef1caaf5f252f3010696a409c27d2f6e69f3230
Maaz Rashid Mateen <maaz.r2001@gmail.com>
2022-07-24 19:15:46 +0300

    Added a reset button, added a function to reset the grid and prompt for grid size

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-const container = document.querySelector('#container')
+const container = document.querySelector('.container')
+const resetSizeBtn = document.querySelector('.reset')
 
-function makeGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--gird-cols', cols);
-    for(i = o; i < (rows * cols); i++){
-        let cell = document.createElement('div')
-        cell.innerText = (c + 1);
-        container.append(cell).className = 'grid-item';
+function makeGrid(size) {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild)
+      } 
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    for (i = 0; i < (size * size); i++) {
+        let cell = document.createElement('div');
+        // cell.innerText = (i + 1);
+        container.appendChild(cell).className = 'grid-item'
+        cell.addEventListener('mouseover', function () {
+            cell.style.backgroundColor = 'black'
+        })
     }
 }
 
-makeGrid(16, 16);
+makeGrid(16)
+
+
+resetSizeBtn.addEventListener('click', resetSize)
+function resetSize() {
+    let gridNum = prompt("what size would you like the grid to be between 1-100:")
+    container.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`
+    makeGrid(gridNum);
+}
